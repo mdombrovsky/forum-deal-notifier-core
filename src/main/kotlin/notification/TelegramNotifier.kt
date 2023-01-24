fce3e650b1e156065b8839b79553745c25f0bd58
Michael Dombrovsky <michaeldombrovsky@gmail.com>
2023-01-24 12:40:47 -0500

    Expanded capability of Telegram notifier to support multiple queries finding the same post

--- src/main/kotlin/notification/TelegramNotifier.kt
+++ src/main/kotlin/notification/TelegramNotifier.kt
@@ -11,20 +11,51 @@ class TelegramNotifier(val apiKey: String) : Notifier {
         token = apiKey
     }
 
-    override suspend fun notify(user: User, query: Query, post: Post) {
+    suspend fun notify(user: User, queries: List<Query>, post: Post) {
         val id = user.retrieveCredential(TelegramId::class.java)
         if (id == null) {
             throw UnsupportedOperationException("Telegram notifications not registered with user")
         } else {
-            val message = if (query.title.trim().isNotEmpty()) {
-                "Matched Query: ${query.title}\n\n${post.toPrettyString()}"
-            } else {
-                post.toPrettyString()
-            }
-            bot.sendMessage(
-                chatId = ChatId.fromId(id.value.toLong()),
-                text = message
-            )
+            /**
+            TODO refactor this title functionality separate function
+            Do this when adding email notifier
+             */
+            val title: String =
+                if (queries.isEmpty()) {
+                    ""
+                } else if (queries.size == 1) {
+                    "Matched Query: ${queries[0].title}"
+                } else {
+                    val allQueryTitles: StringBuilder = StringBuilder()
+                    for (i: Int in 0..queries.lastIndex) {
+                        allQueryTitles.append(queries[i].title)
+                        if (i != queries.lastIndex) {
+                            allQueryTitles.append(", ")
+                        }
+                    }
+                    "Matched Queries: $allQueryTitles"
+                }
+
+            notify(id.value.toLong(), title, post.toPrettyString())
         }
+    }
+
+    override suspend fun notify(user: User, query: Query, post: Post) {
+        notify(user, listOf(query), post)
+    }
+
+    suspend fun notify(user: User, post: Post) {
+        notify(user, listOf(), post)
+    }
+
+    private suspend fun notify(id: Long, title: String = "", postDescription: String) {
+        bot.sendMessage(
+            chatId = ChatId.fromId(id),
+            text = if (title.trim().isNotEmpty()) {
+                "${title}\n\n${postDescription}"
+            } else {
+                postDescription
+            }
+        )
     }
 }
