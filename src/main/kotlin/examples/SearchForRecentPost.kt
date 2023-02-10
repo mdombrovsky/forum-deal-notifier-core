f15bf541c6c41bb26abd72df01fa1ec0ede66f26
Michael Dombrovsky <michaeldombrovsky@gmail.com>
2023-02-10 14:10:03 -0500

    Change example to use RFD

--- src/main/kotlin/examples/SearchForRecentPost.kt
+++ src/main/kotlin/examples/SearchForRecentPost.kt
@@ -3,17 +3,15 @@ package examples
 import notification.stdout.PrintlnNotifier
 import post.PostFinderManager
 import scraper.Scraper
-import scraper.custom.RedditJSONScraper
+import scraper.custom.RFDScraper
 import user.User
 
 
 suspend fun main() {
     val manager = PostFinderManager()
 
-    // This uses SlickDeals RSS feed to alert you of new deals
-    val scraper: Scraper =
-        RedditJSONScraper("bapcsalescanada")
-
+    // This uses the RFD feed to alert you of new deals
+    val scraper: Scraper = RFDScraper()
     val user = User("John")
 
     // MatchAll is a simple query that matches all posts, mostly used for debugging
