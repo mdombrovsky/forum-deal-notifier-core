ec36e0ae5ef260aa8a480c32c8d125a98c10a537
Michael Dombrovsky <michaeldombrovsky@gmail.com>
2024-01-09 23:39:40 -0500

    Add Slickdeals trending scraper

---
+++ src/main/kotlin/scraper/custom/SlickdealsScraper.kt
@@ -0,0 +1,27 @@
+package scraper.custom
+
+import Post
+import org.jsoup.nodes.Element
+import scraper.HtmlWebScraper
+
+class SlickdealsScraper : HtmlWebScraper("", "https://slickdeals.net/") {
+    override fun getPostElementsFromDocument(document: Element): List<Element> {
+        return document.select("li.bp-p-sidebarDeals_deal")
+    }
+
+    override fun createPostFromElement(htmlPost: Element): Post {
+        val titleElement: Element = htmlPost.selectFirst("h4.bp-p-sidebarDeals_title a")!!
+        val title = titleElement.text()
+        val url = titleElement.attr("href")
+
+        return Post(
+            title = title,
+            url = url,
+            source = getName(),
+        )
+    }
+
+    override fun getName(): String {
+        return "Slickdeals trending posts"
+    }
+}
