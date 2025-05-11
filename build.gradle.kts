4633f5750095e937e0153b6d616fdc43ab62ae75
Michael Dombrovsky <michaeldombrovsky@gmail.com>
2025-05-11 16:17:38 -0400

    Upgrade dependencies
    -some packages had vulnerabilities to so update as many packages while avoiding major versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.9.25"
     application
 }
 
@@ -10,25 +8,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven ( url="https://jitpack.io" )
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.21")
+    implementation(kotlin("stdlib-jdk8"))
     implementation("com.rometools:rome:1.18.0")
     implementation("org.slf4j:slf4j-simple:1.7.36")
-    implementation("com.github.kotlin-telegram-bot.kotlin-telegram-bot:dispatcher:6.0.7")
+    implementation("com.github.kotlin-telegram-bot.kotlin-telegram-bot:dispatcher:6.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.json:json:20220924")
-    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.json:json:20250107")
+    implementation("org.jsoup:jsoup:1.20.1")
+    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
